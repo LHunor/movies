9063b4046b3521bb87f38eb421d95c9c3614a074
Hunor Lakatos <lakatoshunor9716@gmail.com>
2024-02-29 10:07:24 +0100

    minor refactor and comments

--- src/services/Watchmode.service.ts
+++ src/services/Watchmode.service.ts
@@ -17,13 +17,13 @@ const search = async (type: SearchType, name: string) => {
   const response = await axios.get(url);
   // The task was to return a single movie or person as a response
   // however the Watchmode API cannot be configured to do an exact match
-  // so it is implemented here
+  // so it is implemented here.
+  // TODO: decide what to do in case of multiple entities with same name property
   const options =
     type === "movie"
       ? response.data?.title_results
       : response.data?.people_results;
-  const exactMatch = options.find((o: { name: string }) => o.name === name);
-  return exactMatch;
+  return options.find((o: { name: string }) => o.name === name);
 };
 
 export default { isTypeValid, isNameValid, search };
